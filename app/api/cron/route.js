3bef9060f1f7f1cf984bcf362725332283664e10
retardcoder <imarinoka@gmail.com>
2026-03-03 04:39:06 +0700

    add force-dynamic to all api routes

--- app/api/cron/route.js
+++ app/api/cron/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server'
+export const dynamic = 'force-dynamic'
 import sql, { initDB } from '../../../lib/db'
 import { fetchMcap } from '../../../lib/dexscreener'
 
--- app/api/nominate/route.js
+++ app/api/nominate/route.js
@@ -1,4 +1,5 @@
 import { NextResponse } from 'next/server'
+export const dynamic = 'force-dynamic'
 import sql, { initDB } from '../../../lib/db'
 import { fetchTokenInfo } from '../../../lib/dexscreener'
 
